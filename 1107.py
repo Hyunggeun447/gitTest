63ea926ea9bd789a2ca734f25025fecda7980ccb
bopi447 <bopi447@naver.com>
2021-12-12 00:24:41 +0900

    메세지

--- 1107.py
+++ 1107.py
@@ -1,12 +1,24 @@
 import sys
-ch=int(input())
-
+ch=str(input())
+ch_list=list(ch)
+for i in range(len(ch)):
+    ch_list[i]=int(ch_list[i])
 k=int(input())
 
-num_break=sys.stdin.readline().rstrip()
+num_break=sys.stdin.readline().rsplit()
+
+for i in range(len(num_break)):
+    num_break[i]=int(num_break[i])
+
+for i in range(len(ch)):
+    while True:
+        num=ch_list[i]
+        if num in num_break:
+            num_1=
+            
 
 
+        
 
-
-
-print(ch,k,num_break)
+print(ch)
+print(num_break)
