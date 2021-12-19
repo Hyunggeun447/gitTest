c29a818333c52fc8f25fab7407eb6dad99ca396c
bopi447 <bopi447@naver.com>
2021-12-19 13:14:38 +0900

    백준_IOI_100점_재공부 요망

--- 5525.py
+++ 5525.py
@@ -7,15 +7,19 @@ N = int(input())
 M = int(input())
 S = str(input().strip())
 
-#print(N, M, S)
-ioi = 'I'
-for _ in range(N):
-    ioi+='OI'
+ans = 0
+cnt = 0
+i = 1
 
-answer = 0
+while i < M-1:
+    if S[i -1] == 'I' and S[i] =='O' and S[i+1] =='I':
+        cnt +=1
+        if cnt == N:
+            cnt -= 1
+            ans += 1
+        i +=1
+    else:
+        cnt = 0
+    i+=1
 
-for i in range(M-len(ioi)):
-    if S[i] == 'I':
-        if S[i:i+len(ioi)] == ioi:
-            answer+=1
-print(answer)
+print(ans)
