40763b6d26c46300d157840fde2c964f29357f95
bopi447 <bopi447@naver.com>
2021-12-20 20:23:11 +0900

    백준_N과M(4)

---
+++ 15652.py
@@ -0,0 +1,21 @@
+# N과 M(4)
+
+import sys
+input = sys.stdin.readline
+
+N , M = map(int, input().split())
+
+list=[]
+
+def dfs_back(start):
+    if len(list) == M:
+        print(' '.join(map(str,list)))
+        return
+    
+    for i in range(start, N+1):
+        # if i not in list:
+        list.append(i)
+        dfs_back(i)
+        list.pop()
+
+dfs_back(1)
