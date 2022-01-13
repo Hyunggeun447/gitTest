f73080f96ea164fbab9cbfecb52a371632d9227b
bopi447 <bopi447@naver.com>
2022-01-13 20:43:03 +0900

    백준:단어변환,딕셔너리(items())활용

---
+++ 1157.py
@@ -0,0 +1,26 @@
+# 단어공부
+
+
+import sys
+input = sys.stdin.readline
+
+word=str(input().strip())
+
+dict={}
+
+for i in word:
+    if i.upper() not in dict:
+        dict[i.upper()]=0
+    dict[i.upper()]+=1
+
+print(dict)
+
+count=0
+ans=""
+for key, value in dict.items():
+    if value>count:
+        count=value
+        ans=key
+    elif value == count:
+        ans="?"
+print(ans,value)
