83b0a6e459baab40dbe06f18ff293f7f62e59c84
bopi447 <bopi447@naver.com>
2022-01-28 18:41:36 +0900

    백준 스터커 dp

---
+++ 9465.py
@@ -0,0 +1,22 @@
+# 스티커 / dp
+
+
+import sys
+input = sys.stdin.readline
+
+t=int(input())
+
+for _ in range(t):
+    n=int(input())
+    arr=[]
+    for _ in range(2):
+        arr.append(list(map(int, input().split())))
+
+    for i in range(1,n):
+        if i == 1:
+            arr[0][i] += arr[1][i-1]
+            arr[1][i] += arr[0][i-1]
+        else:
+            arr[0][i] += max(arr[1][i - 1], arr[1][i - 2])
+            arr[1][i] += max(arr[0][i - 1], arr[0][i - 2])
+    print(max(arr[0][-1],arr[1][-1]))
